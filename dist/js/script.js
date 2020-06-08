a5bb33da5b95cbab1d954f2536c4dc68788f92ba
stanislav <stas.oykin@yandex.ru>
2020-06-08 21:22:09 +0300

    adaptation

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,7 +5,6 @@ const wrap2 = document.querySelector('.main__right-common-wrapper-2');
 arrowBtn.addEventListener('click', () => {
     wrap1.classList.toggle('active-wrap-1');
     wrap2.classList.toggle('active-wrap-2');
-    
 });
 
 
