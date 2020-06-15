449acbceb934f5caceb56a17203119e540a973f6
stanislav <stas.oykin@yandex.ru>
2020-06-15 17:36:01 +0300

    added favicon

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,7 @@
 const arrowBtn = document.getElementById('arrow-label');
 const wrap1 = document.querySelector('.main__right-common-wrapper-1');
 const wrap2 = document.querySelector('.main__right-common-wrapper-2');
+const socialLinks = document.querySelectorAll('.social-links');
 
 arrowBtn.addEventListener('click', () => {
     wrap1.classList.toggle('active-wrap-1');
@@ -8,6 +9,18 @@ arrowBtn.addEventListener('click', () => {
 });
 
 
-
+socialLinks.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        socialLinks.forEach(item => {
+            item.style.opacity = 0.6;
+        });
+        item.style.opacity = 1;
+    });
+    item.addEventListener('mouseout', () => {
+        socialLinks.forEach(item => {
+            item.style.opacity = 1;
+        });
+    });
+});
 
 
